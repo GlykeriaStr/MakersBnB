189185827e8bc899ac8f7dd844c70fa4188ce631
Glykeria Stravodimou <strglykeria@gmail.com>
2021-02-02 17:26:38 +0000

    changes to booking.create

--- lib/booking.rb
+++ lib/booking.rb
@@ -3,23 +3,22 @@ require 'pg'
 
 class Booking
 
-  attr_reader :id, :days, :listing_id
+  attr_reader :id, :days, :listing_id, :user_id
 
-  def initialize(id:, days: ,listing_id:)
+  def initialize(id:, days: ,listing_id:, user_id:)
     @id = id
     @days = days
     @listing_id = listing_id
+    @user_id = user_id
   end
 
-  def self.create(days:)
+  def self.create(listing_id:, user_id: )
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'makersbnb_test')
     else
       connection = PG.connect(dbname: 'makersbnb')
     end
-    result = connection.exec("INSERT INTO bookings (days) VALUES (#{days}) RETURNING id, days, listing_id;").first
-    Booking.new(id: result['id'], days: result['days'], listing_id: result['listing_id'])
+    result = connection.exec("INSERT INTO bookings (listing_id, user_id) VALUES (#{listing_id}, #{user_id}) RETURNING id, days, listing_id, user_id;").first
+    Booking.new(id: result['id'], days: result['days'], listing_id: result['listing_id'], user_id: result['user_id'])
   end
 end
-
-
--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -7,14 +7,16 @@ describe Booking do
 
     it 'should request a booking' do
       user = User.create(email: 'hello@gmail', password: 'password', name: 'meg')
+      user_id = user.id
       listing = Spaces.create(name: 'first listing', description: 'Lovely place', cost: 60, user_id: user.id)
-      booking = Booking.create(days: 2)
+      listing_id = listing.id
+      booking = Booking.create(listing_id: listing_id, user_id: user_id )
       id = booking.id
       persisted_data = persisted_data_retrieve(table: 'bookings', id: id)
 
       expect(booking).to be_a Booking
-      expect(booking.days).to eq '2'
-      # expect(booking.end_date).to eq 2021-02-19
+      expect(booking.listing_id).to eq listing_id
+      expect(booking.user_id).to eq user_id
       expect(booking.id).to eq persisted_data['id']
     end
   end
