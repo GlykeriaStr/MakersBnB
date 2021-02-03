a2f92690b7780cc476d6a5bdb2049017fef28b5d
Reeshul <reeshul@hotmail.com>
2021-02-03 15:08:29 +0000

    added some feature tests

--- spec/feature/homepage_spec.rb
+++ spec/feature/homepage_spec.rb
@@ -1,6 +1,7 @@
 feature "when visiting homepage" do
-  it "a message should appear" do
+  scenario "a message should appear" do
     visit("/")
-    expect(page).to have_content("Hello World")
+    expect(page).to have_content("Makers BnB")
+    expect(page).to have_content("We'll never share your email with anyone else.")
   end
 end
