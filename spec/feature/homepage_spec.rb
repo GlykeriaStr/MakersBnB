2eb779d6e1be037d7297e20d50e9d7520397ddb3
Glykeria Stravodimou <strglykeria@gmail.com>
2021-02-04 10:39:31 +0000

    Glykeria branch

--- spec/feature/homepage_spec.rb
+++ spec/feature/homepage_spec.rb
@@ -1,5 +1,5 @@
 feature "when visiting homepage" do
-  it "a message should appear" do
+  scenario "a message should appear" do
     visit("/")
     expect(page).to have_content("Hello World")
   end
