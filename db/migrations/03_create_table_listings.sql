4825fe7dac8c755db2d8f3a684a298c19e3c95b7
Glykeria Stravodimou <strglykeria@gmail.com>
2021-02-04 15:14:07 +0000

    updated the db migrations

--- db/migrations/03_create_table_listings.sql
+++ db/migrations/03_create_table_listings.sql
@@ -1,2 +1,1 @@
-CREATE TABLE listings(id SERIAL PRIMARY KEY, name VARCHAR(60), description VARCHAR(180), cost INT);
-ALTER TABLE listings ADD user_id INTEGER REFERENCES users(id);
+CREATE TABLE listings(id SERIAL PRIMARY KEY, name VARCHAR(60), description VARCHAR(180), cost INT,user_id INTEGER REFERENCES users(id), Availability boolean DEFAULT true);
--- db/migrations/04_create_table_bookings.sql
+++ db/migrations/04_create_table_bookings.sql
@@ -1,1 +1,1 @@
-CREATE TABLE bookings (id SERIAL PRIMARY KEY, listing_id INTEGER REFERENCES listings(id), user_id INTEGER REFERENCES users(id), Availability boolean DEFAULT true);
+CREATE TABLE bookings (id SERIAL PRIMARY KEY, listing_id INTEGER REFERENCES listings(id), user_id INTEGER REFERENCES users(id));
